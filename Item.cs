0d170324643c80b91b232e8e6d1bf0578157eaa2
TrahsPanda <zupdike17@gmail.com>
2022-06-08 00:14:11 -0400

    Add project files.

---
+++ Item.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonCrawler
+{
+    internal class Item
+    {
+        internal static byte next = 0;
+        public byte id = next++;
+        public string name;
+        public byte quality;
+        public byte defense;
+        public byte damage;
+        public byte condition;
+
+        public Item()
+        {
+            id = 0;
+            name = "debug";
+            quality = 0;
+            defense = 0;
+            damage = 0;
+            condition = 0;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonCrawler
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Debug testing :)\n\n Type item to spawn a random item");
+            string input = Console.ReadLine();
+            if (input == "item")
+            {
+                input = "";
+                new Item();
+            }
+        }
+    }
+}
